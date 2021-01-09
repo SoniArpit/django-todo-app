96177181754e5300927e552ea2b9e4f7b18bfcdb
SoniArpit <arpitsoni362@gmail.com>
2021-01-09 21:04:42 +0530

    pagination

--- todolist/api/views.py
+++ todolist/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from ..models import Task
 from .serializers import TodoSerailizer
+from rest_framework.pagination import PageNumberPagination
 
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TodoSerailizer
+    pagination_class = PageNumberPagination
+    PageNumberPagination.page_size = 10
